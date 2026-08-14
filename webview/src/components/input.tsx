f1cf70c427004b8b00fcbf8beeae93f393f4a9b8
huanent <huanent@icloud.com>
2026-08-15 01:22:53 +0800

    refactor: standardize webview controls and dialog styling

--- webview/src/components/input.tsx
+++ webview/src/components/input.tsx
@@ -1,13 +1,13 @@
 import type { InputHTMLAttributes, SelectHTMLAttributes, TextareaHTMLAttributes } from 'react';
 
-export const inputClassName = 'w-full border border-[var(--vscode-input-border,var(--vscode-widget-border,var(--vscode-panel-border)))] bg-(--vscode-input-background) text-(--vscode-input-foreground) outline-none focus:border-(--vscode-focusBorder)';
+export const inputClassName = 'w-full rounded-[2px] border border-[var(--vscode-input-border,var(--vscode-widget-border,var(--vscode-panel-border)))] bg-(--vscode-input-background) text-(--vscode-input-foreground) outline-none focus:border-(--vscode-focusBorder) disabled:bg-(--vscode-input-background) disabled:text-(--vscode-disabledForeground,var(--vscode-descriptionForeground)) disabled:opacity-70';
 
 export function TextInput({ className = '', ...props }: InputHTMLAttributes<HTMLInputElement>) {
 	return <input className={`${inputClassName} h-8.5 px-2.5 ${className}`} {...props} />;
 }
 
 export function SelectInput({ className = '', ...props }: SelectHTMLAttributes<HTMLSelectElement>) {
-	return <select className={`${inputClassName} h-8.5 px-2.5 ${className}`} {...props} />;
+	return <select className={`${inputClassName} h-8.5 px-2.5 text-(--vscode-dropdown-foreground) ${className}`} {...props} />;
 }
 
 export function TextArea({ className = '', ...props }: TextareaHTMLAttributes<HTMLTextAreaElement>) {
